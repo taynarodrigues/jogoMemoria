e5e481215d0768c6e629da70d55b75201d6cbf48
Tayna Rodrigues <taynajesus@gmail.com>
2019-09-01 12:57:16 -0400

    implementando rotação das figuras

--- script.js
+++ script.js
@@ -9,6 +9,18 @@
            card.style.left=  i % 8 === 0 ? + 5 + "px" : i%8 * 165 + 5 + "px";
            card.style.top = i < 8 ? 5 + "px" : 250 + "px";
            alert();
+
+           //Atribuir evento do movimento da caixa
+
+           card.addEventListener("click", flipCard, false);
         }
+    }
+
+    function flipCard(){
+        var faces = this.getElementsByClassName("face");
+        /*console.log(faces[0]);*/
+        faces[0].classList.toggle("flipped");
+        faces[1].classList.toggle("flipped");
+        /*console.log(faces[0].classList);*/
     }
 }());
