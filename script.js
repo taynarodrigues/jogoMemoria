fae45b0c52825ea4d9492e8f570c4da5b947d0a5
Tayna Rodrigues <taynajesus@gmail.com>
2019-09-01 16:59:58 -0400

    organizando as imagens de forma aleatória

--- script.js
+++ script.js
@@ -9,7 +9,9 @@
     }
     console.log(imagens);
 
-    startGame();
+    startGame();// responsável pela inicialização do jogo
+    /*chamada da função para embaralhar as figuras*/
+         imagens =randomSort(imagens);  //organização aleatória
 
     function startGame(){
 
@@ -18,11 +20,9 @@
         for(var i = 0; i < 16; i++){
             var card = document.querySelector("#card" + i);
             console.log( i % 8);
-            /*card.getElementsByClassName.left = 500 + "px";*/
-           card.style.left=  i % 8 === 0 ?  5 + "px" : i % 8 * 165 + 5 + "px";
+           card.style.left =  i % 8 === 0 ?  5 + "px" : i % 8 * 165 + 5 + "px";
            card.style.top = i < 8 ? 5 + "px" : 250 + "px";
-        //   alert();
-
+       
            //Atribuir evento do movimento das figuras
 
            card.addEventListener("click", flipCard, false);
@@ -33,6 +33,27 @@
         }
     }
 
+    function randomSort(oldArray){
+        Math.floor(Math.random()*11);
+        var arrTeste = ["uva", "morango", "goiaba"];
+        console.log(arrTeste.length);
+
+        var newArray = [];
+        /*Estrutura de repetição 1-Criar um array vazio
+        2-Avaliar o número de elementos do array
+        3-Criar um índice com valor aleatório
+        4-Avaliar se o elemento indicado já existe nonovo array
+        5-Inserir o elemento indicado no novo array */
+        while(newArray.length != oldArray.length){
+            var i = Math.floor(Math.random()*oldArray.length);
+
+            if(newArray.indexOf(oldArray[i]) < 0){
+                newArray.push(oldArray[i]);
+           }
+        }
+        return newArray;
+    }
+    
     function flipCard(){
         var faces = this.getElementsByClassName("face");
         /*console.log(faces[0]);*/
