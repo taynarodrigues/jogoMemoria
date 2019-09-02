71cbc1f963d1e8d86085ec943010080c15f8af8c
Tayna Rodrigues <taynajesus@gmail.com>
2019-09-02 00:20:29 -0400

    reajustando novas implementações de alguns bugs no código

--- script.js
+++ script.js
@@ -39,10 +39,10 @@
 
             var card = document.querySelector("#card" + i);
             console.log(i % 8);
-            
+
             card.style.left = (i % 8) === 0 ? 5 + "px" : 5 + ((i % 8) * 165) + "px";
-            card.style.top = i/8 >= 1 ? 250 + "px" : 5 + "px";
-            
+            card.style.top = i / 8 >= 1 ? 250 + "px" : 5 + "px";
+
             //Atribuir evento do movimento das figuras
 
             card.addEventListener("click", flipCard, false);
@@ -56,34 +56,14 @@
         modalGameOver.removeEventListener("click", startGame, false);
     }
 
-    function randomSort(oldArray) {
-        Math.floor(Math.random() * 11);
-        var arrTeste = ["uva", "morango", "goiaba"];
-        console.log(arrTeste.length);
-
-        var newArray = [];
-        /*Estrutura de repetição 1-Criar um array vazio
-        2-Avaliar o número de elementos do array
-        3-Criar um índice com valor aleatório
-        4-Avaliar se o elemento indicado já existe nonovo array
-        5-Inserir o elemento indicado no novo array */
-        while (newArray.length != oldArray.length) {
-            var i = Math.floor(Math.random() * oldArray.length);
-
-            if (newArray.indexOf(oldArray[i]) < 0) {
-                newArray.push(oldArray[i]);
-            }
-        }
-        return newArray;
-    }
-
     function flipCard() {
         if (flippedCards.length < 2) {
             var faces = this.getElementsByClassName("face");
 
             console.log(faces[0].classList.length);
 
-            if (faces[0].classList.length > 2) {//aborta a execução dos métodos-> nõa chega a virar as imagens
+            //aborta a execução dos métodos-> nõa chega a virar as imagens
+            if (faces[0].classList[2]) {
                 return;
             }
             /*console.log(faces[0]);*/
@@ -92,7 +72,7 @@
             /*console.log(faces[0].classList);*/
             flippedCards.push(this);
             if (flippedCards.length === 2) {
-                if (flipCard[0].childNodes[3].id === flippedCards[1].childNodes[3].id) {
+                if (flippedCards[0].childNodes[3].id === flippedCards[1].childNodes[3].id) {
                     flippedCards[0].childNodes[1].classList.toggle("match");
                     flippedCards[0].childNodes[3].classList.toggle("match");
                     flippedCards[1].childNodes[1].classList.toggle("match");
@@ -100,11 +80,11 @@
 
                     matchCardSign();
 
-                    matches++;
-
                     flippedCards = [];
 
-                    if (matches === 8) {
+                    matches++;
+
+                    if (matches >= 8) {
                         gameOver();
                     }
                 }
@@ -124,19 +104,42 @@
            gameOver();
        },1000);
    */
-    function gameOver() {
-        modalGameOver.style.zIndex = 10;
-        modalGameOver.addEventListener("click", startGame, false);
+
+
+    function randomSort(oldArray) {
+
+        var newArray = [];
+        /*Estrutura de repetição 1-Criar um array vazio
+        2-Avaliar o número de elementos do array
+        3-Criar um índice com valor aleatório
+        4-Avaliar se o elemento indicado já existe nonovo array
+        5-Inserir o elemento indicado no novo array */
+        while (newArray.length != oldArray.length) {
+            var i = Math.floor(Math.random() * oldArray.length);
+
+            if (newArray.indexOf(oldArray[i]) < 0) {
+                newArray.push(oldArray[i]);
+            }
+        }
+        return newArray;
     }
+
+
     function matchCardSign() {
-        imgMatchSign.style.zIndex = 1;
+        imgMatchSign.style.zIndex = "1";
         imgMatchSign.style.top = 150 + "px";
-        imgMatchSign.style.opacity = 0;
+        imgMatchSign.style.opacity = "0";
         setTimeout(function () {
-            imgMatchSign.style.zIndex = -1;
+            imgMatchSign.style.zIndex = "-1";
             imgMatchSign.style.top = 250 + "px";
-            imgMatchSign.style.opacity = 1;
+            imgMatchSign.style.opacity = "1";
         }, 1500);
+    }
+    function gameOver() {
+        modalGameOver.style.zIndex = "99";
+        modalGameOver.addEventListener("click", function () {
+            startGame();
+        }, false);
     }
 
 }());
